3dc6b2f35cb714b4b26d584445b1d081aa11bbf5
Маг Ильяс DOMA <157958205+MagIlyas-DOMA@users.noreply.github.com>
2025-01-18 14:50:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 desc = '\n'.join(("Универсальная библиотека python для большинства задач", 'A universal python library for most tasks'))
 req = '''soundfile==0.13.0
@@ -11,20 +11,20 @@ beautifulsoup4==4.12.3
 pyodbc==5.2.0'''.split('\n')
 
 setup(
-    name='hrenpack',  # Название вашей библиотеки
-    version='0.1.0',  # Версия
+    name='hrenpack',
+    version='1.0.0',
     author_email='hrenpack@mail.ru',
-    author='Маг Ильяс DOMA (MagIlyas_DOMA)',  # Авторы
+    author='Маг Ильяс DOMA (MagIlyas_DOMA)',
     description=desc,  # Описание
-    long_description=open('README.md').read(),  # Долгое описание (обычно из README)
-    long_description_content_type='text/markdown',  # Формат долгого описания
-    url='https://github.com/MagIlyas-DOMA/hrenpack',  # URL репозитория
-    packages=['hrenpack'],  # Найдите пакеты автоматически
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MagIlyas-DOMA/hrenpack',
+    packages=find_packages(),
     license=open('LICENSE.md').read(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.10',  # Минимальная версия Python
-    install_requires=req  # Зависимости
+    python_requires='>=3.10',
+    install_requires=req
 )
