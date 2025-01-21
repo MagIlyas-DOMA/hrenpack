0fc422225e9a4aa74c4929fadb9bae8862bba1f4
Маг Ильяс DOMA <157958205+MagIlyas-DOMA@users.noreply.github.com>
2025-01-21 20:15:21 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 from setuptools import setup, find_packages
 
 desc = '\n'.join(("Универсальная библиотека python для большинства задач", 'A universal python library for most tasks'))
-req = '''soundfile==0.13.0
-SpeechRecognition==3.14.0
-requests>=2.32.3
-screeninfo>=0.8.1
-psutil>=6.1.1
-bs4==0.0.2
-beautifulsoup4==4.12.3
-pyodbc==5.2.0'''.split('\n')
+req = open('requirements.txt').read().split('\n')
+
 
 setup(
     name='hrenpack',
-    version='1.0.0',
+    version='1.1.1',
     author_email='hrenpack@mail.ru',
     author='Маг Ильяс DOMA (MagIlyas_DOMA)',
-    description=desc,  # Описание
-    long_description=open('README.md', encoding='utf-8').read(),
+    description=desc,
+    license='BSD 3-Clause License',
+    long_description=open('README_PIP.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/MagIlyas-DOMA/hrenpack',
     packages=find_packages(),
-    license=open('LICENSE.md').read(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
